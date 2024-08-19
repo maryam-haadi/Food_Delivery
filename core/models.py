5a767635267e756734edbfb6482fe934f3347280
Maryam <maryamhaaadi8077@gmail.com>
2024-08-19 20:40:08 +0330

    new changes

--- core/models.py
+++ core/models.py
@@ -63,7 +63,7 @@ class Restaurant(models.Model):
     latitude = models.FloatField(default=0)
     longitude = models.FloatField(default=0)
     is_open = models.BooleanField(default=False,blank=True)
-    average_rating = models.FloatField(blank=True,default=None)
+    average_rating = models.FloatField(blank=True,default=None,null=True)
 
     def save(self, *args, **kwargs):
         current_time = datetime.now().time()
@@ -138,7 +138,7 @@ class Food(models.Model):
     name = models.CharField(max_length=150,blank=False)
     desc = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10,decimal_places=2,blank=False)
-    average_rating = models.FloatField(blank=True,default=None)
+    average_rating = models.FloatField(blank=True,default=None,null=True)
 
     def __str__(self):
         return f"food name : {self.name} with id : {self.id} for restaurant : {self.menu.restaurant.name}"
